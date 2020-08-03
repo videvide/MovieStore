e8d17515ceeb73093398e0e6d9643c3df19ad032
Vide Krajnc <krajncvide53@gmail.com>
2020-08-03 14:30:58 +0200

    Added models for database

---
+++ MovieStore.Lib/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Lib.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime OrderDate { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ MovieStore.Lib/Models/OrderRows.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Lib.Models
+{
+    public class OrderRows
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        [Required]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+
+    }
+}
