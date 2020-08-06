804f7dfa1267ba4697ae6a547fbea51c2249c628
Sebastian Vallin <sebbe.vallin@gmail.com>
2020-08-06 12:43:18 +0200

    Fix connection string & added admin controller

---
+++ MovieStore.Web/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MovieStore.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieStore.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void AddToAdmin()
+        {
+            var roles = _context.Roles.ToList();
+
+            foreach (var role in roles)
+            {
+                Response.Write($"Role: {role.Name}<br/>");
+            }
+        }
+    }
+}
