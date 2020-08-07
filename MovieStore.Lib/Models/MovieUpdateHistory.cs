d13de2a6a982c3c56a2d57efd4d7515f55e3c77f
Vide Krajnc <krajncvide53@gmail.com>
2020-08-07 12:37:55 +0200

    Added movie update history model and migration
    - Made update movie history model with properties.
    - Added dbSet property in DbContext.
    - Added migration.

---
+++ MovieStore.Lib/Models/MovieUpdateHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Lib.Models
+{
+    public class MovieUpdateHistory
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime LastUpdate { get; set; }
+    }
+}
