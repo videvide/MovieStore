6067d024819611ecb496c3f96d31afc4d1797126
Sebastian Vallin <sebbe.vallin@gmail.com>
2020-08-07 12:21:43 +0200

    Added Methods to get top 100 movies by rating
    - Added GetTop100MovieIds method.
    - Added GetMovieById method.
    - Added GetTop100Movies method.
    - Added model for MovieIds Response.

--- MovieStore.Lib/DataAccess/OMDBDataAccess.cs
+++ MovieStore.Lib/DataAccess/OMDBDataAccess.cs
@@ -33,6 +33,64 @@ namespace MovieStore.Lib.DataAccess
                 throw new Exception("Error");
             }
         }
+        public async Task<MovieResponse> GetMovieById(string id)
+        {
+            var result = await _client.GetAsync($"{API_URI}i={id}");
+            if (result.IsSuccessStatusCode)
+            {
+                var r = await result.Content.ReadAsStringAsync();
+                MovieResponse movieData = JsonConvert.DeserializeObject<MovieResponse>(r);
+                return movieData;
+            }
+            else
+            {
+                throw new Exception("Error");
+            }
+        }
+
+        public async Task<List<MovieIdsResponse>> GetTop100MovieIds()
+        {
+            string URL = "https://imdb8.p.rapidapi.com/title/get-top-rated-movies/";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "imdb8.p.rapidapi.com");
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "4d61e7bdb2msh8a6a096b8f281cdp122f8djsn35ddf37bbbfc");
+
+            var result = await client.GetAsync(URL);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var r = await result.Content.ReadAsStringAsync();
+
+                var data = JsonConvert.DeserializeObject<List<MovieIdsResponse>>(r).Take(100).ToList();
+
+                return data;
+            }
+
+            else throw new Exception("Error fetching data");
+        }
+
+        public async Task<List<Movie>> GetTop100Movies()
+        {
+            var movieIdList = await GetTop100MovieIds();
+
+            List<Movie> output = new List<Movie>(); 
+            foreach (var movieId in movieIdList)
+            {
+                var m = await GetMovieById(movieId.id.Replace("/title/", "").Replace("/", "").Trim());
+                var movie = new Movie
+                {
+                    Title = m.Title,
+                    Director = m.Director,
+                    Price = 0,
+                    ReleaseYear = m.Year
+                };
+
+                output.Add(movie);
+            }
+
+            return output;
+        }
     }
 }
 
---
+++ MovieStore.Lib/Models/MovieIdsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Lib.Models
+{
+    public class MovieIdsResponse
+    {
+        public string id { get; set; }
+        public float chartRating { get; set; }
+    }
+}
