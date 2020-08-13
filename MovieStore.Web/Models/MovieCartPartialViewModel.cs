a46ed1f51f4f6c244f8d5cfc8e4195aaf3f8285c
Sebastian <sebbe.vallin@gmail.com>
2020-08-13 20:43:11 +0200

    Updated MovieCart Partial view
    - Updated Style.
    - Added logo to navbar.
    - Removed Jumbotron in "Mobile" view.
    - Updated Cart view and partial view.
    - Added MovieCartPartialViewModel.

---
+++ MovieStore.Web/Models/MovieCartPartialViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MovieStore.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Web.Models
+{
+    public class MovieCartPartialViewModel
+    {
+        public Movie Movie { get; set; }
+        public int Count { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
