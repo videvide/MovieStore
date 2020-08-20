bd85abec5c1a5b730f4d55b6fa2acf1baec034e0
Sebastian <sebbe.vallin@gmail.com>
2020-08-20 20:31:06 +0200

    Updated Checkout View Model DisplayNames

--- MovieStore.Web/Models/CheckOutViewModel.cs
+++ MovieStore.Web/Models/CheckOutViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MovieStore.Lib.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -21,38 +22,55 @@ namespace MovieStore.Web.Models
 
         [Required]
         [MaxLength(100)]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
         [Required]
         [MaxLength(255)]
+        [DisplayName("Billing Address")]
         public string BillingAddress { get; set; }
+
         [Required]
         [MaxLength(100)]
+        [DisplayName("Billing City")]
         public string BillingCity { get; set; }
+
         [Required]
         [DataType(DataType.PostalCode)]
+        [DisplayName("Billing Zip Code")]
         public string BillingZip { get; set; }
+
         [Required]
         [MaxLength(255)]
+        [DisplayName("Delivery Address")]
         public string DeliveryAddress { get; set; }
+
         [Required]
         [MaxLength(100)]
+        [DisplayName("Delivery City")]
         public string DeliveryCity { get; set; }
+
         [Required]
         [DataType(DataType.PostalCode)]
+        [DisplayName("Delivery Zip Code")]
         public string DeliveryZip { get; set; }
 
         [Required]
         [EmailAddress]
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
+
         [Required]
         [Phone]
+        [DisplayName("Phone Number")]
         public string PhoneNo { get; set; }
+
         public string ApplicationUserId { get; set; }
         public virtual ApplicationUser ApplicationUser { get; set; }
-
     }
 }
