1a52b3581efb4b6193d42b8182aa43983e00aa87
Sebastian <sebbe.vallin@gmail.com>
2020-08-20 20:22:07 +0200

    Updated Customer Model with DisplayNames

--- MovieStore.Lib/Models/Customer.cs
+++ MovieStore.Lib/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -14,35 +15,45 @@ namespace MovieStore.Lib.Models
 
         [Required]
         [MaxLength(100)]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
         [MaxLength(255)]
+        [DisplayName("Billing Address")]
         public string BillingAddress { get; set; }
 
         [MaxLength(100)]
+        [DisplayName("Billing City")]
         public string BillingCity { get; set; }
 
         [DataType(DataType.PostalCode)]
+        [DisplayName("Billing Zip Code")]
         public string BillingZip { get; set; }
 
         [MaxLength(255)]
+        [DisplayName("Deliver Address")]
         public string DeliveryAddress { get; set; }
 
         [MaxLength(100)]
+        [DisplayName("Delivery City")]
         public string DeliveryCity { get; set; }
 
         [DataType(DataType.PostalCode)]
+        [DisplayName("Postal Zip Code")]
         public string DeliveryZip { get; set; }
 
         [Required]
         [EmailAddress]
+        [DisplayName("Email Address")]
         public string EmailAddress { get; set; }
 
         [Phone]
+        [DisplayName("Phone Number")]
         public string PhoneNo { get; set; }
 
         public string ApplicationUserId { get; set; }
